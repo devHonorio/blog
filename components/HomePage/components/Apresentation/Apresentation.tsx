61e8c121e718168298e9583beebd5d6d1f545ea4
José Honorio de Oliveira Sobrinho Neto <honorio.dev@gmail.com>
2024-07-31 22:15:42 -0300

    style

--- components/HomePage/components/Apresentation/Apresentation.tsx
+++ components/HomePage/components/Apresentation/Apresentation.tsx
@@ -4,7 +4,9 @@ export const Apresentation = () => {
   return (
     <S.container id="tags">
       <S.title>Portifólio</S.title>
-      <p>Veja meus projetos e como pode ajudar seu negócio</p>
+      <p className="text-center">
+        Veja meus projetos e como pode ajudar seu negócio
+      </p>
     </S.container>
   );
 };
